36c385504b9a4bf667a76d4530d845852e87df71
Bickeringmonkey <smeadows110280@gmail.com>
2021-08-06 12:43:10 +0100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const playlist = [
+    ['Bad', 'Michael Jackson', '2:05'],
+    ['Amazed By You', 'Lonestar', '4:02'],
+    ['Lego House', 'Ed Sheran', '3:25'],
+    ['Evita', 'Madonna', '5:02'],
+    ['Truely, Madly, Deeply', 'Savage Garden', '2:23'],
+    ['Lifted', 'The Lighthouse Family', '3:02']
+  ];
+
+  function createListItems(arr) {
+    let items = '';
+    for ( let i = 0; i < arr.length; i++) {
+        items += `<li>${arr[i][0]}, By ${arr[i][1]} - ${arr[i][2]}</li>`;
+    }
+    return items;
+  }
+
+  document.querySelector('main').innerHTML = `
+  <lo>
+    ${createListItems(playlist)}
+  </lo>
+  `;
